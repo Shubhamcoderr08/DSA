dcb99aaf1faac56223d2c4929f2432d017a6ec2e
Shubhamcoderr08 <shubhamjha0834@gmail.com>
2026-03-08 11:45:46 +0530

    DSA day 8

---
+++ Arrays/majority_element.cpp
@@ -0,0 +1,68 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int majorityElement(vector<int> &nums){
+
+//     int n = nums.size();
+
+//     for(int i=0;i<n;i++){
+
+//         int count = 0;
+
+//         for(int j=0;j<n;j++){
+//             if(nums[j] == nums[i]){
+//                 count++;
+//             }
+//         }
+
+//         if(count > n/2){
+//             return nums[i];
+//         }
+//     }
+
+//     return -1;
+// }
+
+// int main(){
+
+//     vector<int> nums = {2,2,1,1,1,2,2};
+
+//     cout<<"Majority Element = "<<majorityElement(nums);
+
+// }
+
+// optimal approach
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int majorityElement(vector<int> &nums){
+
+    int count = 0;
+    int candidate = 0;
+
+    for(int num : nums){
+
+        if(count == 0){
+            candidate = num;
+        }
+
+        if(num == candidate){
+            count++;
+        }
+        else{
+            count--;
+        }
+    }
+
+    return candidate;
+}
+
+int main(){
+
+    vector<int> nums = {2,2,1,1,1,2,2};
+
+    cout<<"Majority Element = "<<majorityElement(nums);
+
+}
