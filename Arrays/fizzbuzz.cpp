c6c25373476e5fd3863264a1b16531a2be910ddb
Shubhamcoderr08 <shubhamjha0834@gmail.com>
2026-03-31 14:19:47 +0530

    DSA....

---
+++ Arrays/fizzbuzz.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n = 15;
+
+    for(int i = 1; i <= n; i++) {
+        if(i % 3 == 0 && i % 5 == 0) cout << "FizzBuzz";
+        else if(i % 3 == 0) cout << "Fizz";
+        else if(i % 5 == 0) cout << "Buzz";
+        else cout << i << " ";
+    }
+
+    return 0;
+}
