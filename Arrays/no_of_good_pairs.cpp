7ef534eb59fcd2d180aaeb72acef6192155fe6f4
Shubhamcoderr08 <shubhamjha0834@gmail.com>
2026-04-02 21:31:22 +0530

    DSA...

---
+++ Arrays/no_of_good_pairs.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+     vector<int> nums={0,1,1,3,5,6};
+
+     int n = nums.size();
+     int count  = 0;
+     for(int i =0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if(nums[i]==nums[j]){
+              count++;
+            }
+        }
+     }
+
+     cout<<"Number of Good Pairs:"<<count;
+}
