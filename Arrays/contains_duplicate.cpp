268e27609fcb63ae58b3721bd6717a5420c30d19
Shubhamcoderr08 <shubhamjha0834@gmail.com>
2026-04-06 17:39:56 +0530

    DSA....

--- Arrays/contains_duplicate.cpp
+++ Arrays/contains_duplicate.cpp
@@ -1,18 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    vector<int> nums = {1, 2, 3, 1};
+// int main() {
+//     vector<int> nums = {1, 2, 3, 1};
 
-    sort(nums.begin(), nums.end());
+//     sort(nums.begin(), nums.end());
 
-    for (int i = 1; i < nums.size(); i++) {
-        if (nums[i] == nums[i - 1]) {
-            cout << "true";
-            return 0;
-        }
-    }
+//     for (int i = 1; i < nums.size(); i++) {
+//         if (nums[i] == nums[i - 1]) {
+//             cout << "true";
+//             return 0;
+//         }
+//     }
 
-    cout << "false";
-    return 0;
-}
+//     cout << "false";
+//     return 0;
+// }
+
+// Optimissed Solution 
+
