4289ded85f6b5b196d7b366c39d2d05007fb8329
Shubhamcoderr08 <shubhamjha0834@gmail.com>
2026-04-11 12:29:23 +0530

    DSA...

--- Arrays/moves_zerroes_to_end.cpp
+++ Arrays/moves_zerroes_to_end.cpp
@@ -1,24 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void moveZeroes(vector<int>& nums) {
+// void moveZeroes(vector<int>& nums) {
+//     int j = 0;
+
+//     for(int i = 0; i < nums.size(); i++){
+//         if(nums[i] != 0){
+//             swap(nums[i], nums[j]);
+//             j++;
+//         }
+//     }
+// }
+
+// int main() {
+//     vector<int> nums = {0,1,0,3,12};
+
+//     moveZeroes(nums);
+
+//     for(int x : nums){
+//         cout << x << " ";
+//     }
+
+//     return 0;
+// }
+
+// Moves zeroes to the Front !!!
+
+void move_Zeroes_Front(vector<int> &nums)
+{
+
     int j = 0;
 
-    for(int i = 0; i < nums.size(); i++){
-        if(nums[i] != 0){
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (nums[i] == 0)
+        {
             swap(nums[i], nums[j]);
             j++;
         }
     }
 }
 
-int main() {
-    vector<int> nums = {0,1,0,3,12};
+int main()
+{
 
-    moveZeroes(nums);
+    vector<int> nums = {1, 2, 0, 2, 0, 13};
 
-    for(int x : nums){
-        cout << x << " ";
+    move_Zeroes_Front(nums);
+
+    int n = nums.size();
+    for (int i = 0; i < n; i++)
+    {
+        cout << nums[i] << " ";
     }
 
     return 0;
--- strings/valid_anagram.cpp
+++ strings/valid_anagram.cpp
@@ -1,22 +1,99 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
 
+// brute Force
+// int main(){
+
+//     string s = "anagram";
+//     string t = "nagaram";
+
+//     if(s.length() != t.length()){
+//         cout << "false";
+//         return 0;
+//     }
+
+//     sort(s.begin(), s.end());
+//     sort(t.begin(), t.end());
+
+//     if(s == t)
+//         cout << "true";
+//     else
+//         cout << "false";
+
+// }
+
+// hashmap
+
+
+// int main(){
+// string s = "anagram";
+// string t = "nagaram";
+
+//  if(s.length() != t.length()){
+//         cout << "false";
+//         return 0;
+//     }
+// unordered_map<char,int>mp;
+
+//     for(char c:s){
+//         mp[c]++;
+//     }
+
+//     for(char c:t){
+//         mp[c]--;
+//     }
+
+//     for(auto it:mp){
+//         if(it.second!=0) {
+//             cout<<"false";
+//              return 0;
+//         } 
+//     }
+
+
+//     cout<<"true";
+
+//     return 0;
+
+// }
+
+
+// More Optimissed:
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool isAnagram(string s, string t) {
+    if(s.length() != t.length()) return false;
+
+    vector<int> freq(26, 0);
+
+    for(int i = 0; i < s.length(); i++){
+        freq[s[i] - 'a']++;   // add
+        freq[t[i] - 'a']--;   // subtract
+    }
+
+    for(int x : freq){
+        if(x != 0) return false;
+    }
+
+    return true;
+}
+
+int main() {
     string s = "anagram";
     string t = "nagaram";
 
-    if(s.length() != t.length()){
-        cout << "false";
-        return 0;
+    if(isAnagram(s, t)){
+        cout << "True (Anagram)" << endl;
+    } else {
+        cout << "False (Not Anagram)" << endl;
     }
 
-    sort(s.begin(), s.end());
-    sort(t.begin(), t.end());
-
-    if(s == t)
-        cout << "true";
-    else
-        cout << "false";
-
+    return 0;
 }
+
+
